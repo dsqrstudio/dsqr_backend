a144daabfb4f5930f6060236bfa0c1a54fa2c234
Pavan Suthar <sutharpavan9153@gmail.com>
2026-02-10 14:14:50 +0530

    ddec

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,9 +2,11 @@
 
 import redis from 'redis'
 
+
 const client = redis.createClient({
-  host: 'localhost',
-  port: 6379,
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined, // Only set if needed
 })
 
 client.on('error', (err) => {
